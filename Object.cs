72adf8d1c4fbb74251c49c105126872a8da59b2e
PJSanto429 <86988750+PJSanto429@users.noreply.github.com>
2022-11-28 17:01:18 -0500

    initial commit

---
+++ Object.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Game
+{
+    internal class Object
+    {
+        private string name;
+        private string longName;
+        private string description;
+        private string takeable;
+        public string inInventory;
+        public string room;
+
+        public Object(
+            string Aname,
+            string AlongName,
+            string Adescription,
+            string Aroom,
+            string Atakeable,
+            string AinInventory
+        )
+        {
+            name = Aname;
+            longName = AlongName;
+            description = Adescription;
+            room = Aroom;
+            takeable = Atakeable;
+            inInventory = AinInventory;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string LongName
+        {
+            get { return longName; }
+            set { longName = value; }
+
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public string Takeable
+        {
+            get { return takeable; }
+            set { takeable = value; }
+        }
+    }
+
+    internal class Player
+    {
+        public string room = "first";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿// Peyton Santo
+// This is a text game engine. originally made in Python, translated into C#
+// started - 11/28/2022
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace Text_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Startup();
+            Player player = new Player();
+            while (true)
+            {
+                GetInput();
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Startup()
+        {
+            Console.WriteLine("Welcome to 'Text Game Engine'");
+        }
+
+        static void GetInput()
+        {
+            Console.Write(">> ");
+            string userInput = Console.ReadLine();
+            string[] splitUserInput = userInput.Split(new char[] { });
+            //prints the user input in a readable way
+            //for (int i = 0; i < splitUserInput.Length; i++)
+            //{
+            //    Console.Write("{0} ", splitUserInput[i], i);
+            //}
+            //Console.WriteLine("\n-----------------------");
+
+            if (userInput.Length > 1)
+            {
+
+            }
+            else // if user types one word
+            {
+
+            }
+
+            //return userInput;
+        }
+    }
+}
